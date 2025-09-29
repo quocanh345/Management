b9334c5f64eb9141e5fda94e1587824556e92f96
Abel <quocanh80@gmail.com>
2025-09-29 16:35:33 +0000

    test CI

--- sources/app.py
+++ sources/app.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "hello, it's work!"
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
--- test/test_route.py
+++ test/test_route.py
@@ -2,7 +2,7 @@ from src.app import app
 
 client = app.test_client()
 
+
 def test_home():
     res = client.get('/')
     assert res.status_code == 200
-
